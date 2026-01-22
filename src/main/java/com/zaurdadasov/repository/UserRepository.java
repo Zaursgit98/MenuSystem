0ef7445809c9d0a4c4496527442291e9784c79f7
zaur's git <dadasovzaur93@gmail.com>
2026-01-22 11:25:26 +0400

    Remove files from dev branch

--- src/main/java/com/zaurdadasov/repository/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.zaurdadasov.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.zaurdadasov.model.User;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-
-}
--- src/main/java/com/zaurdadasov/service/IChatService.java
+++
@@ -1,5 +0,0 @@
-package com.zaurdadasov.service;
-
-public interface IChatService {
-
-}
